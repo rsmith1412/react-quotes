a0b2717388b31e83be812b71c3d19c8bc54e1f1c
Robert Smith <robertsmith1@Roberts-MacBook-Pro.local>
2017-08-04 16:29:01 -0700

    another one

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   // This is the entry point or start of our react applicaton
-  entry: "./app/App.js",
+  entry: "./app/app.js",
 
   // The plain compiled Javascript will be output into this file
   output: {
